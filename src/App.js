d3f68750fb41d6c3d24150d24e2f3313eaba8e52
revyark <anantiy42@gmail.com>
2025-04-22 23:11:27 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import AIChat from './components/AIChat'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AIChat/>
     </div>
   );
 }
